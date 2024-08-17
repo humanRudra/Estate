d420c45e80124546cb5dd7d1fa750fe90ba897fa
Bhanu Pratap Singh Pundeer <bhanupratapsinghpundeer@Bhanus-Air.bbrouter>
2024-08-17 15:59:15 +0530

    create pages and add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
   return (
-   <>APP STart</>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
